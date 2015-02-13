38054d47d0aec8745f7ade5fe18cbdeb430b6b57
Sergey Tyatin <700@2rba.com>
2015-02-13 15:14:10 +0200

    some ember files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,13 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require handlebars
+//= require ember
+//= require ember-data
+//= require_self
+//= require edemo
 //= require_tree .
+
+window.Edemo = Ember.Application.create()
