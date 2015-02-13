90b2c7fa75a1e8b1ae522af7733b17d7d95a1a1c
Sergey Tyatin <700@2rba.com>
2015-02-13 16:46:01 +0200

    ember route added

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,5 +1,10 @@
 // For more information see: http://emberjs.com/guides/routing/
 
 Edemo.Router.map(function(){
-    this.resource('posts')
-})
+    this.resource('rows')
+});
+Edemo.IndexRoute = Ember.Route.extend({
+  beforeModel: function() {
+    this.transitionTo('rows');
+  }
+});
